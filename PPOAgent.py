e8ac5cef8ea80a2a2bbdec837855443c0b2868c5
«razbiralochka» <razbiralochka@yandex.ru>
2026-03-27 20:16:30 +0300

    CommitF

--- PPOAgent.py
+++ PPOAgent.py
@@ -10,13 +10,16 @@ from collections import deque
 class Crtic(nn.Module):
     def __init__(self):
         super(Crtic, self).__init__()
-        self.fc1 = nn.Linear(9, 64)
-        self.fc2 = nn.Linear(64, 1)
-        self.relu = torch.nn.ReLU()
+        self.fc1 = nn.Linear(9, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.sp = torch.nn.Softplus()
+        self.tanh = nn.Tanh()
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        x = self.sp(self.fc1(x))
+        x = self.sp(self.fc2(x))
+        x = self.sp(self.fc3(x))
+        return self.tanh(x)
 
 class Actor(nn.Module):
     def __init__(self):
@@ -24,12 +27,11 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(9, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 9)
-        self.relu = torch.nn.ReLU()
         self.sp = torch.nn.Softplus()
     def forward(self, x):
         x = self.sp(self.fc1(x))
         x = self.sp(self.fc2(x))
-        x = self.sp(self.fc3(x))
+        x = self.fc3(x)
         probs = torch.softmax(x, dim=-1)
         return probs
 
@@ -38,8 +40,8 @@ class PPOAgent:
     def __init__(self):
         self.modelA = Actor()
         self.modelC = Crtic()
-        self.optimizerA = optim.SGD(self.modelA.parameters(), lr=0.0002)
-        self.optimizerC = optim.SGD(self.modelC.parameters(), lr=0.002)
+        self.optimizerA = optim.SGD(self.modelA.parameters(), lr=0.001)
+        self.optimizerC = optim.Adam(self.modelC.parameters(), lr=0.002)
         self.curr_prob = 0
         self.curr_trac = list()
         self.trajcs = list()
@@ -52,6 +54,8 @@ class PPOAgent:
             valid_actions = [i for i, cell in enumerate(state) if cell == 0]
             action = random.choice(valid_actions)
             self.curr_prob = np.ones(9)/9.0
+            self.modelA = Actor()
+            print("NAN")
             return action
         self.curr_prob = probs.detach().numpy()[0]
         action_dist = torch.distributions.Categorical(probs=probs)
@@ -68,7 +72,7 @@ class PPOAgent:
     def rememberTraj(self):
         self.trajcs.append(self.curr_trac.copy())
         self.curr_trac.clear()
-        if len(self.trajcs) > 50:
+        if len(self.trajcs) > 150:
             self.trajcs.clear()
 
     def learn(self):
@@ -80,49 +84,48 @@ class PPOAgent:
         for traj in self.trajcs:
             L = len(traj)
             for i in reversed(range(L)):
-
                 if i < L-1:
                     traj[i][3] = traj[i][3] + traj[i+1][3]
 
             for elem in traj:
                 data.append(elem)
-            random.shuffle(data)
+        random.shuffle(data)
 
-            Rlist = list()
 
-            for epoch in range(1):
-                for line in data:
-                    state, act, prob, R = line
-                    Rlist.append(R)
-                    state = torch.tensor(state,dtype=torch.float).unsqueeze(0)
-                    Rew = torch.tensor(R,dtype=torch.float).reshape([1,1])
+        for epoch in range(50):
+            for line in data:
+                state, act, prob, R = line
+                stateT = torch.tensor(state,dtype=torch.float)
+                Rew = torch.tensor(R,dtype=torch.float).unsqueeze(0)
 
+                V = self.modelC(stateT)
+
+
+                loss = nn.MSELoss()(V, Rew)
+                self.optimizerC.zero_grad()
+                loss.backward()
+                self.optimizerC.step()
+
+
+        for epoch in range(5):
+
+            for line in data:
+                state, act, oldProbs, R = line
+                state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
+                Rew = torch.tensor(R, dtype=torch.float).unsqueeze(0)
+                oldProbs = torch.tensor(oldProbs, dtype=torch.float).unsqueeze(0)
+
+                with torch.no_grad():
                     V = self.modelC(state)
-                    loss = nn.MSELoss()(V, Rew)
-                    self.optimizerC.zero_grad()
-                    loss.backward()
-                    self.optimizerC.step()
+                A = (Rew-V).detach().item()
 
-            baseline = np.mean(Rlist)
+                self.optimizerA.zero_grad()
+                probs = self.modelA(state)[0][act]
 
-            for epoch in range(3):
-                for line in data:
-                    state, act, oldProbs, R = line
-                    state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
-                    Rew = torch.tensor(R, dtype=torch.float).unsqueeze(0)
-                    oldProbs = torch.tensor(oldProbs, dtype=torch.float).unsqueeze(0)
+                ratio = torch.exp(torch.log(probs) - torch.log(oldProbs))
+                loss = -torch.min(A*ratio, torch.clamp(ratio, 0.8, 1.2))
 
-                    with torch.no_grad():
-                        V = self.modelC(state)
-                    A = (Rew-baseline).detach().item()
-
-
-                    probs = self.modelA(state)[0][act]
-
-                    ratio = torch.exp(torch.log(probs) - torch.log(oldProbs))
-                    loss = -torch.min(A*ratio, torch.clamp(ratio, 0.8, 1.2))
-                    self.optimizerA.zero_grad()
-                    loss.backward()
-                    self.optimizerA.step()
+                loss.backward()
+                self.optimizerA.step()
 
         self.trajcs.clear()
